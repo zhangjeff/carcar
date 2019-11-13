3524b34b9fa6bf39190634d24016836440976d1d
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2019-11-13 17:20:48 +0800

    commit

--- src/main/resources/public/jquery/mockzhih/mockzhih.js
+++ src/main/resources/public/jquery/mockzhih/mockzhih.js
@@ -9,7 +9,24 @@ $(function () {
         contentType: 'application/json',
         // data: JSON.stringify(param),
         success: function (result) {
+            result.forEach(function(item, index){
+                var sectionVal = "";
+                if (item.section_list) {
+                    item.section_list.forEach(function(subitem, index){
+                        sectionVal = sectionVal + "<span>" + subitem.section_title + "</span>";
+                    });
+                }
 
+                var  objHtml = $("#template").html()
+                               .replace("srcurl", item.banner)
+                               .replace("titleVal", item.title)
+                               .replace("contentVal", item.introduction)
+                               .replace("senctionList", sectionVal)
+                               .replace("viewCountVal", item.view_count);
+
+
+                $(".allList").append(objHtml);
+            });
            console.table([1111,result, 2222]);
 
         },
